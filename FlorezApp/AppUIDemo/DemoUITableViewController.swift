a3a9f658403529f071ed55421a14627d27dfc788
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-01-21 19:01:18 -0600

    Add new DemoSection and DemoView enums and the array to show in the tableview

--- FlorezApp/AppUIDemo/DemoUITableViewController.swift
+++ FlorezApp/AppUIDemo/DemoUITableViewController.swift
@@ -7,84 +7,97 @@
 
 import UIKit
 
+enum DemoSection {
+    case components
+    case logInFeature
+    
+    var nameSection: String {
+        switch self {
+        case .components:
+            return "Components"
+        case .logInFeature:
+            return "LogIn Feature"
+        }
+    }
+}
+
+enum DemoView {
+    /// Enum to show only the UI Components (ej. Buttons, Labels, TableViews).
+    case buttons
+    case labels
+    
+    /// Enum to show only the features
+    ///
+    /// LogInFeature
+    case homeScreenViewController
+    
+    
+    /// FeedFeature
+    ///
+    ///
+    ///
+    
+    var nameView: String {
+        switch self {
+        case .buttons:
+            return "Buttons"
+        case .labels:
+            return "Labels"
+        case .homeScreenViewController:
+            return "HomeScreenViewController"
+        }
+    }
+}
+
 class DemoUITableViewController: UITableViewController {
+    typealias DemoList = (section: DemoSection, views: [DemoView])
+    
+    private var demoViews = [DemoList]()
         
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        /// Components Demo
+        demoViews.append(
+            (.components, 
+             [
+                .buttons,
+                .labels
+             ]
+            )
+        )
+        
+        /// LogInFeature Demo
+        demoViews.append(
+            (.logInFeature, 
+             [
+                .homeScreenViewController
+             ]
+            )
+        )
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return demoViews.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return demoViews[section].views.count
     }
 
-    /*
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return demoViews[section].section.nameSection
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = UITableViewCell()
+        var config = cell.defaultContentConfiguration()
+        config.text = demoViews[indexPath.section].views[indexPath.row].nameView
+        cell.contentConfiguration = config
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
 }
