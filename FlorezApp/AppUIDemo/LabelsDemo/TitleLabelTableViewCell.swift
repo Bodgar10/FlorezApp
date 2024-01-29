e26df9f148d131e67a026cec28ad96bc7676282b
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-01-28 19:35:42 -0600

    S

---
+++ FlorezApp/AppUIDemo/LabelsDemo/TitleLabelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleLabelTableViewCell.swift
+//  FlorezApp
+//
+//  Created by Bodgar Espinosa Miranda on 28/01/24.
+//
+
+import UIKit
+
+class TitleLabelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FlorezApp/UIComponents/Styles/LabelStyles/CustomLabel.swift
@@ -0,0 +1,8 @@
+//
+//  CustomLabel.swift
+//  FlorezApp
+//
+//  Created by Bodgar Espinosa Miranda on 28/01/24.
+//
+
+import Foundation
