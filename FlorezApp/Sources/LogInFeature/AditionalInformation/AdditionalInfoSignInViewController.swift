dc5e49b21fb085d1d82104ea6c393a8e021f4093
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-02-11 19:48:39 -0600

    Modified all the additional info view controller

--- FlorezApp/Sources/LogInFeature/AditionalInformation/AdditionalInfoSignInViewController.swift
+++ FlorezApp/Sources/LogInFeature/AditionalInformation/AdditionalInfoSignInViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class AdditionalInfoSignInViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        
+        nameTextField.delegate = self
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
     }
 
+}
+
+extension AdditionalInfoSignInViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
 }
