9f3ff4fa1666a0a5636975187c2de30823405f0e
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2024-02-11 18:35:41 -0600

    Add new view controller for more information

---
+++ FlorezApp/Sources/LogInFeature/AditionalInformation/AdditionalInfoSignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionalInfoSignInViewController.swift
+//  FlorezApp
+//
+//  Created by Bodgar Espinosa Miranda on 11/02/24.
+//
+
+import UIKit
+
+class AdditionalInfoSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
